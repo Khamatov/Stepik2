c6414d72f9d2eba0897ecfca6a3f9b541ca1c979
Khamatov <spirid2398@gmail.com>
2023-04-26 19:16:02 +0300

    Добавил функцию для проверки совпадения результатов

--- module 2/2.2/anflat_EC.py
+++ module 2/2.2/anflat_EC.py
@@ -3,11 +3,20 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 import time
+
 link = "https://anflat.ru/"
 browser = webdriver.Chrome()
 browser.maximize_window()
-
+url = browser.current_url
 browser.get(link)
+#Селекторы:
+mainH1 = browser.find_element(By.CSS_SELECTOR, "h1 div")
+mainH1Text = mainH1.text
+expectedH1Text = "Купить квартиру в Казани - подбор объектов"
+#Функция для проверки
+def test_input_text(expected_result, actual_result):
+    assert expected_result != actual_result, f"expected {expected_result}, got {actual_result}"
+
 
 # говорим Selenium проверять в течение 2 секунд, пока баннер с куками не станет кликабельным
 buttonCloseCoockie = WebDriverWait(browser, 2).until(
@@ -22,13 +31,14 @@ buttonCloseTimepadBanner = WebDriverWait(browser, 10).until(
 buttonCloseTimepadBanner.click()
 print("Таймпадовский баннер нашёлся и закрылся")
 
-buttonLtxwidgetClose = WebDriverWait(browser, 220).until(
-        EC.element_to_be_clickable((By.CSS_SELECTOR, ".lt-xwidget .lt-xwidget-close i"))
-    )
-buttonLtxwidgetClose.click()
-print("Лайвтеховский баннер нашёлся и закрылся")
-time.sleep(10)
-mainText = browser.find_element(By.CSS_SELECTOR, "h1 div")
+# buttonLtxwidgetClose = WebDriverWait(browser, 220).until(
+#         EC.element_to_be_clickable((By.CSS_SELECTOR, ".lt-xwidget .lt-xwidget-close i"))
+#     )
+# buttonLtxwidgetClose.click()
+# print("Лайвтеховский баннер нашёлся и закрылся")
+# time.sleep(10)
 
-assert "Купить квартиру в Казани - подбор объектов" in mainText.text
-time.sleep(10)
+assert expectedH1Text == mainH1Text, f"Строка {mainH1Text} не равна {expectedH1Text}"
+print("Строка h1 найдена")
+time.sleep(2)
+test_input_text(expectedH1Text, mainH1Text)
